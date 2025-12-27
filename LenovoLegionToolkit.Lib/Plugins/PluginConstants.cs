e163cbc1c68089fb2e4a1e97f7d01b643d7c061a
crs102597 <crs102597@noreply.gitcode.com>
2025-12-27 18:57:07 +0800

    chore: Remove other plugin dependencies, keep only network plugin

--- LenovoLegionToolkit.Lib/Plugins/PluginConstants.cs
+++ LenovoLegionToolkit.Lib/Plugins/PluginConstants.cs
@@ -6,7 +6,5 @@ namespace LenovoLegionToolkit.Lib.Plugins;
 public static class PluginConstants
 {
     public const string NetworkAcceleration = "NetworkAcceleration";
-    public const string AiAssistant = "AiAssistant";
-    public const string DockLauncher = "DockLauncher";
 }
 
