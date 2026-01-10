eb1e0030d84434cf3e25546b846a80392df9a60c
crs102597 <crs102597@noreply.gitcode.com>
2026-01-10 10:47:20 +0800

    fix: Fix duplicate WTS entries in NativeMethods.txt and CLI validator logic

--- make.bat
+++ make.bat
@@ -11,11 +11,11 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 msbuild Shell.sln /p:Configuration=release /p:Platform=x64 /m
 popd
 
-@REM SET PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
+SET PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
 dotnet build .\ShellIntegration\Shell.Build.csproj -c Release || exit /b
 dotnet publish LenovoLegionToolkit.WPF -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
 dotnet publish LenovoLegionToolkit.SpectrumTester -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
 dotnet publish LenovoLegionToolkit.CLI -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
 dotnet build LenovoLegionToolkit.Plugins.NetworkAcceleration -c Release /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
 
-@REM iscc make_installer.iss /DMyAppVersion=%VERSION% || exit /b
+iscc make_installer.iss /DMyAppVersion=%VERSION% || exit /b
