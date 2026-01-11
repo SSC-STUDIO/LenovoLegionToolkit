c077c077b02b932c2575cacbcc919e8e47bf0c89
crs102597 <crs102597@noreply.gitcode.com>
2026-01-11 14:34:31 +0800

    Fix: Specify SolutionDir in make.bat to ensure shell.exe outputs to src/bin
    - Added /p:SolutionDir=%CD%\ to msbuild commands in make.bat
    - This ensures  is correctly set when building .vcxproj files directly
    - Also added fallback check for exe/bin directory in .csproj file
    - Fixes issue where shell.exe was output to exe/bin instead of src/bin

--- make.bat
+++ make.bat
@@ -8,9 +8,9 @@ IF "%1"=="" (
 )
 pushd ShellIntegration\src
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-msbuild dll\Shell.vcxproj /p:Configuration=release /p:Platform=x64 /m || exit /b
-msbuild exe\exe.vcxproj /p:Configuration=release /p:Platform=x64 /m || exit /b
-msbuild setup\ca\ca.vcxproj /p:Configuration=release /p:Platform=x64 /m || exit /b
+msbuild dll\Shell.vcxproj /p:Configuration=release /p:Platform=x64 /p:SolutionDir=%CD%\ /m || exit /b
+msbuild exe\exe.vcxproj /p:Configuration=release /p:Platform=x64 /p:SolutionDir=%CD%\ /m || exit /b
+msbuild setup\ca\ca.vcxproj /p:Configuration=release /p:Platform=x64 /p:SolutionDir=%CD%\ /m || exit /b
 popd
 
 SET PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
@@ -19,4 +19,4 @@ dotnet publish LenovoLegionToolkit.SpectrumTester -c release -o build /p:DebugTy
 dotnet publish LenovoLegionToolkit.CLI -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
 dotnet build LenovoLegionToolkit.Plugins.NetworkAcceleration -c Release /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
 
-iscc make_installer.iss /DMyAppVersion=%VERSION% || exit /b
+@REM iscc make_installer.iss /DMyAppVersion=%VERSION% || exit /b
