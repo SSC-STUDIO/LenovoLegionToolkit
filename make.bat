d0dff1c5b471fe46c21b95b5f2f704a4d4a4c601
crs102597 <crs102597@noreply.gitcode.com>
2026-01-17 00:22:00 +0800

    fixed errors

--- make.bat
+++ make.bat
@@ -21,9 +21,9 @@ if exist "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\
 ) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 )
-@REM msbuild dll\Shell.vcxproj /p:Configuration=release /p:Platform=x64 /p:SolutionDir=%CD%\ /m || exit /b
-@REM msbuild exe\exe.vcxproj /p:Configuration=release /p:Platform=x64 /p:SolutionDir=%CD%\ /m || exit /b
-@REM msbuild setup\ca\ca.vcxproj /p:Configuration=release /p:Platform=x64 /p:SolutionDir=%CD%\ /m || exit /b
+msbuild dll\Shell.vcxproj /p:Configuration=release /p:Platform=x64 /p:SolutionDir=%CD%\ /m || exit /b
+msbuild exe\exe.vcxproj /p:Configuration=release /p:Platform=x64 /p:SolutionDir=%CD%\ /m || exit /b
+msbuild setup\ca\ca.vcxproj /p:Configuration=release /p:Platform=x64 /p:SolutionDir=%CD%\ /m || exit /b
 popd
 
 SET PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
@@ -33,4 +33,4 @@ dotnet publish LenovoLegionToolkit.CLI -c release -o build /p:DebugType=None /p:
 dotnet build LenovoLegionToolkit.Plugins.NetworkAcceleration -c Release /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
 dotnet build LenovoLegionToolkit.Plugins.ViveTool -c Release /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
 
-@REM iscc make_installer.iss /DMyAppVersion=%VERSION% || exit /b
+iscc make_installer.iss /DMyAppVersion=%VERSION% || exit /b
