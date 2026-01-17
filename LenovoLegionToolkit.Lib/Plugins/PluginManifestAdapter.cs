c73daf28d22fa3b26f0cb8b7bc66af86dc6bf1d0
crs102597 <crs102597@noreply.gitcode.com>
2026-01-17 21:30:42 +0800

    Fix: Add PluginManifestAdapter to convert PluginManifest to IPlugin

---
+++ LenovoLegionToolkit.Lib/Plugins/PluginManifestAdapter.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace LenovoLegionToolkit.Lib.Plugins;
+
+/// <summary>
+/// Adapter class to convert PluginManifest to IPlugin
+/// </summary>
+public class PluginManifestAdapter : IPlugin
+{
+    private readonly PluginManifest _manifest;
+
+    public PluginManifestAdapter(PluginManifest manifest)
+    {
+        _manifest = manifest;
+    }
+
+    public string Id => _manifest.Id;
+    public string Name => _manifest.Name;
+    public string Description => _manifest.Description;
+    public string Icon => _manifest.Icon;
+    public bool IsSystemPlugin => _manifest.IsSystemPlugin;
+    public string[]? Dependencies => _manifest.Dependencies;
+}
