dd4d1e687336b22248ab448cac4b06a68ad64bf0
crs102597 <crs102597@noreply.gitcode.com>
2026-02-25 18:03:52 +0800

    fix: Update GitHub username from Crs10259 to SSC-STUDIO
    - Fixed plugin store URLs in PluginRepositoryService.cs
    - Fixed update checker URLs in UpdateChecker.cs
    - Fixed constants in Constants.cs

--- LenovoLegionToolkit.WPF/Constants.cs
+++ LenovoLegionToolkit.WPF/Constants.cs
@@ -6,7 +6,7 @@ public static class Constants
 {
     // Update repository configuration - modify these to change update source
     // Also update the same constants in UpdateChecker.cs in the Lib project
-    public const string UpdateRepositoryOwner = "Crs10259";
+    public const string UpdateRepositoryOwner = "SSC-STUDIO";
     public const string UpdateRepositoryName = "LenovoLegionToolkit";
     public static readonly Uri LatestReleaseUri = new($"https://github.com/{UpdateRepositoryOwner}/{UpdateRepositoryName}/releases/latest");
 
