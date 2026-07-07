2f56aecbfd201ba2ec917e9fd3556bb5b440ca15
SSC-STUDIO <139300395+SSC-STUDIO@users.noreply.github.com>
2026-07-08 02:07:15 +0800

    test: add BackendSyncModels and DeviceSupportCatalog/DevicePack/DeviceFeatureAvailability tests (18 new tests)

---
+++ UniversalDeviceToolkit.Tests/BackendSyncModelsTests.cs
@@ -0,0 +1,138 @@
+﻿using FluentAssertions;
+using LenovoLegionToolkit.Lib;
+using Xunit;
+
+namespace UniversalDeviceToolkit.Tests;
+
+[Trait("Category", TestCategories.Unit)]
+public class BackendSyncModelsTests
+{
+    #region AmdWmiCommand Tests
+
+    [Fact]
+    public void AmdWmiCommand_Init_ShouldSetAllProperties()
+    {
+        var cmd = new AmdWmiCommand { Name = "TestCmd", Id = 0xDEADBEEF, IsSet = true };
+        cmd.Name.Should().Be("TestCmd");
+        cmd.Id.Should().Be(0xDEADBEEF);
+        cmd.IsSet.Should().BeTrue();
+    }
+
+    [Fact]
+    public void AmdWmiCommand_Default_ShouldBeDefault()
+    {
+        var cmd = new AmdWmiCommand();
+        cmd.Name.Should().BeNull();
+        cmd.Id.Should().Be(0);
+        cmd.IsSet.Should().BeFalse();
+    }
+
+    [Fact]
+    public void AmdWmiCommand_ToString_ShouldFormatHexId()
+    {
+        var cmd = new AmdWmiCommand { Name = "MyCmd", Id = 255, IsSet = false };
+        cmd.ToString().Should().Contain("0x000000FF");
+        cmd.ToString().Should().Contain("MyCmd");
+    }
+
+    #endregion
+
+    #region FanSpeedTable Tests
+
+    [Fact]
+    public void FanSpeedTable_Constructor_ShouldSetAllFields()
+    {
+        var table = new FanSpeedTable(1200, 2400, 3600);
+        table.CpuFanSpeed.Should().Be(1200);
+        table.GpuFanSpeed.Should().Be(2400);
+        table.PchFanSpeed.Should().Be(3600);
+    }
+
+    [Fact]
+    public void FanSpeedTable_ZeroValues_ShouldWork()
+    {
+        var table = new FanSpeedTable(0, 0, 0);
+        table.CpuFanSpeed.Should().Be(0);
+        table.GpuFanSpeed.Should().Be(0);
+        table.PchFanSpeed.Should().Be(0);
+    }
+
+    #endregion
+
+    #region HidDeviceConfig Tests
+
+    [Fact]
+    public void HidDeviceConfig_Constructor_ShouldSetAllFields()
+    {
+        var cfg = new HidDeviceConfig(0x1234, 0x5678, 0x0001, 0x0002, "Test Device");
+        cfg.VendorId.Should().Be(0x1234);
+        cfg.ProductId.Should().Be(0x5678);
+        cfg.UsagePage.Should().Be(0x0001);
+        cfg.Usage.Should().Be(0x0002);
+        cfg.DisplayName.Should().Be("Test Device");
+    }
+
+    #endregion
+
+    #region OverclockingProfile Tests
+
+    [Fact]
+    public void OverclockingProfile_Init_ShouldSetFields()
+    {
+        var profile = new OverclockingProfile
+        {
+            FMax = 3000,
+            CoreValues = new List<double?> { 1.2, null, 1.1 }
+        };
+        profile.FMax.Should().Be(3000);
+        profile.CoreValues.Should().HaveCount(3);
+        profile.CoreValues[1].Should().BeNull();
+    }
+
+    [Fact]
+    public void OverclockingProfile_NullFMax_ShouldBeNull()
+    {
+        var profile = new OverclockingProfile();
+        profile.FMax.Should().BeNull();
+        profile.CoreValues.Should().BeNull();
+    }
+
+    #endregion
+
+    #region ShutdownInfo Tests
+
+    [Fact]
+    public void ShutdownInfo_Init_ShouldSetFields()
+    {
+        var info = new ShutdownInfo { Status = "OK", AbnormalCount = 3 };
+        info.Status.Should().Be("OK");
+        info.AbnormalCount.Should().Be(3);
+    }
+
+    [Fact]
+    public void ShutdownInfo_Default_ShouldHaveDefaults()
+    {
+        var info = new ShutdownInfo();
+        info.Status.Should().BeNull();
+        info.AbnormalCount.Should().Be(0);
+    }
+
+    #endregion
+
+    #region KeyMap Tests
+
+    [Fact]
+    public void KeyMap_Constructor_ShouldSetAllFields()
+    {
+        var keyCodes = new ushort[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+        var additional = new ushort[] { 7, 8 };
+        var map = new KeyMap(3, 2, keyCodes, additional);
+
+        map.Width.Should().Be(3);
+        map.Height.Should().Be(2);
+        map.KeyCodes.Should().BeSameAs(keyCodes);
+        map.AdditionalKeyCodes.Should().BeSameAs(additional);
+    }
+
+    #endregion
+}
---
+++ UniversalDeviceToolkit.Tests/DeviceSupportModelsTests.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using LenovoLegionToolkit.Lib.DeviceSupport;
+using Xunit;
+
+namespace UniversalDeviceToolkit.Tests;
+
+[Trait("Category", TestCategories.Unit)]
+public class DeviceSupportModelsTests
+{
+    #region DevicePack Tests
+
+    [Fact]
+    public void DevicePack_Init_ShouldSetAllProperties()
+    {
+        var pack = new DevicePack
+        {
+            Id = "lenovo-legion-y9000p",
+            DisplayName = "Lenovo Legion Y9000P",
+            Vendor = "Lenovo",
+            VendorAliases = new List<string> { "LenovoPC" },
+            Families = new List<string> { "Legion" },
+            ModelPrefixes = new List<string> { "82JD", "82JU" },
+            ModelKeywords = new List<string> { "Y9000P" },
+            MachineTypes = new List<string> { "RTCN5M" },
+            EnabledFeatures = new List<string> { "GodMode", "FnLock" },
+            HiddenFeatures = new List<string> { "TestFeature" }
+        };
+
+        pack.Id.Should().Be("lenovo-legion-y9000p");
+        pack.DisplayName.Should().Be("Lenovo Legion Y9000P");
+        pack.Vendor.Should().Be("Lenovo");
+        pack.VendorAliases.Should().Contain("LenovoPC");
+        pack.Families.Should().Contain("Legion");
+        pack.ModelPrefixes.Should().HaveCount(2);
+        pack.ModelKeywords.Should().Contain("Y9000P");
+        pack.MachineTypes.Should().Contain("RTCN5M");
+        pack.EnabledFeatures.Should().Contain("GodMode");
+        pack.HiddenFeatures.Should().Contain("TestFeature");
+    }
+
+    [Fact]
+    public void DevicePack_DefaultCollections_ShouldBeEmpty()
+    {
+        var pack = new DevicePack { Id = "test", DisplayName = "Test", Vendor = "V" };
+        pack.VendorAliases.Should().BeEmpty();
+        pack.Families.Should().BeEmpty();
+        pack.ModelPrefixes.Should().BeEmpty();
+        pack.ModelKeywords.Should().BeEmpty();
+        pack.MachineTypes.Should().BeEmpty();
+        pack.EnabledFeatures.Should().BeEmpty();
+        pack.HiddenFeatures.Should().BeEmpty();
+    }
+
+    #endregion
+
+    #region DeviceFeatureAvailability Tests
+
+    [Fact]
+    public void DeviceFeatureAvailability_Supported_ShouldBeTrue()
+    {
+        var avail = new DeviceFeatureAvailability
+        {
+            IsSupported = true,
+            DevicePackId = "test-pack",
+            EnabledFeatures = new List<string> { "FnLock" },
+            HiddenFeatures = new List<string>()
+        };
+
+        avail.IsSupported.Should().BeTrue();
+        avail.IsBasicMode.Should().BeFalse();
+        avail.DevicePackId.Should().Be("test-pack");
+        avail.EnabledFeatures.Should().Contain("FnLock");
+    }
+
+    [Fact]
+    public void DeviceFeatureAvailability_NotSupported_IsBasicMode()
+    {
+        var avail = new DeviceFeatureAvailability { IsSupported = false };
+        avail.IsBasicMode.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DeviceFeatureAvailability_Default_ShouldHaveEmptyCollections()
+    {
+        var avail = new DeviceFeatureAvailability();
+        avail.IsSupported.Should().BeFalse();
+        avail.DevicePackId.Should().BeNull();
+        avail.EnabledFeatures.Should().BeEmpty();
+        avail.HiddenFeatures.Should().BeEmpty();
+    }
+
+    #endregion
+
+    #region DeviceSupportCatalog Tests
+
+    [Fact]
+    public void DeviceSupportCatalog_DefaultValues_ShouldBeCorrect()
+    {
+        var catalog = new DeviceSupportCatalog();
+        catalog.SchemaVersion.Should().Be(1);
+        catalog.AppVersion.Should().Be("0.0.0");
+        catalog.DevicePacks.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void DeviceSupportCatalog_Init_ShouldSetAllProperties()
+    {
+        var packs = new List<DevicePack>
+        {
+            new() { Id = "pack1", DisplayName = "Pack 1", Vendor = "V" }
+        };
+
+        var catalog = new DeviceSupportCatalog
+        {
+            SchemaVersion = 2,
+            AppVersion = "1.0.0",
+            DevicePacks = packs
+        };
+
+        catalog.SchemaVersion.Should().Be(2);
+        catalog.AppVersion.Should().Be("1.0.0");
+        catalog.DevicePacks.Should().HaveCount(1);
+    }
+
+    #endregion
+}
