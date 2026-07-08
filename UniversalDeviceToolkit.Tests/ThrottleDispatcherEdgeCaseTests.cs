53e0974020945f12f05306dce8cb974d914c5402
SSC-STUDIO <139300395+SSC-STUDIO@users.noreply.github.com>
2026-07-08 09:43:32 +0800

    test: add 12 ThrottleFirst/LastDispatcher edge-case tests
    - ThrottleFirstDispatcherEdgeCaseTests: 6 tests covering negative interval,
      zero interval, null task, first call execution, second call throttling, reset
    - ThrottleLastDispatcherEdgeCaseTests: 6 tests covering negative interval,
      zero interval, null task, object disposal, task execution, multiple dispose

---
+++ UniversalDeviceToolkit.Tests/ThrottleDispatcherEdgeCaseTests.cs
@@ -0,0 +1,135 @@
+﻿using FluentAssertions;
+using LenovoLegionToolkit.Lib.Utils;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UniversalDeviceToolkit.Tests;
+
+public class ThrottleFirstDispatcherEdgeCaseTests
+{
+    [Fact]
+    public void Constructor_NegativeInterval_Throws()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new ThrottleFirstDispatcher(TimeSpan.FromSeconds(-1)));
+    }
+
+    [Fact]
+    public void Constructor_ZeroInterval_IsAllowed()
+    {
+        var dispatcher = new ThrottleFirstDispatcher(TimeSpan.Zero);
+        dispatcher.Interval.Should().Be(TimeSpan.Zero);
+    }
+
+    [Fact]
+    public async Task DispatchAsync_NullTask_Throws()
+    {
+        var dispatcher = new ThrottleFirstDispatcher(TimeSpan.FromMinutes(1));
+        await Assert.ThrowsAsync<ArgumentNullException>(() => dispatcher.DispatchAsync(null!));
+    }
+
+    [Fact]
+    public async Task DispatchAsync_FirstCall_ExecuteImmediately()
+    {
+        var dispatcher = new ThrottleFirstDispatcher(TimeSpan.FromMinutes(1));
+        var executed = false;
+
+        await dispatcher.DispatchAsync(() =>
+        {
+            executed = true;
+            return Task.CompletedTask;
+        });
+
+        executed.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task DispatchAsync_SecondCallWithinInterval_IsThrottled()
+    {
+        var dispatcher = new ThrottleFirstDispatcher(TimeSpan.FromHours(1));
+        var callCount = 0;
+
+        await dispatcher.DispatchAsync(() =>
+        {
+            callCount++;
+            return Task.CompletedTask;
+        });
+
+        await dispatcher.DispatchAsync(() =>
+        {
+            callCount++;
+            return Task.CompletedTask;
+        });
+
+        callCount.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task ResetAsync_AllowsNextDispatch()
+    {
+        var dispatcher = new ThrottleFirstDispatcher(TimeSpan.FromHours(1));
+        var callCount = 0;
+
+        await dispatcher.DispatchAsync(() => { callCount++; return Task.CompletedTask; });
+        await dispatcher.ResetAsync();
+        await dispatcher.DispatchAsync(() => { callCount++; return Task.CompletedTask; });
+
+        callCount.Should().Be(2);
+    }
+}
+
+public class ThrottleLastDispatcherEdgeCaseTests
+{
+    [Fact]
+    public void Constructor_NegativeInterval_Throws()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new ThrottleLastDispatcher(TimeSpan.FromSeconds(-1)));
+    }
+
+    [Fact]
+    public void Constructor_ZeroInterval_IsAllowed()
+    {
+        var dispatcher = new ThrottleLastDispatcher(TimeSpan.Zero);
+        dispatcher.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task DispatchAsync_NullTask_Throws()
+    {
+        using var dispatcher = new ThrottleLastDispatcher(TimeSpan.FromMinutes(1));
+        await Assert.ThrowsAsync<ArgumentNullException>(() => dispatcher.DispatchAsync(null!));
+    }
+
+    [Fact]
+    public async Task DispatchAsync_AfterDispose_ThrowsObjectDisposed()
+    {
+        var dispatcher = new ThrottleLastDispatcher(TimeSpan.FromMinutes(1));
+        dispatcher.Dispose();
+
+        await Assert.ThrowsAsync<ObjectDisposedException>(() => dispatcher.DispatchAsync(() => Task.CompletedTask));
+    }
+
+    [Fact]
+    public async Task DispatchAsync_ExecuteTask()
+    {
+        using var dispatcher = new ThrottleLastDispatcher(TimeSpan.Zero);
+        var executed = false;
+
+        await dispatcher.DispatchAsync(() =>
+        {
+            executed = true;
+            return Task.CompletedTask;
+        });
+
+        executed.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Dispose_CanBeCalledMultipleTimes()
+    {
+        var dispatcher = new ThrottleLastDispatcher(TimeSpan.FromMinutes(1));
+        dispatcher.Dispose();
+        dispatcher.Dispose();
+    }
+}
