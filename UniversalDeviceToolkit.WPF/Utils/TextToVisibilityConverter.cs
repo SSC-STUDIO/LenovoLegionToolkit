bab986cb23bae707c07d1c623b46edabd625cdbd
SSC-STUDIO <139300395+SSC-STUDIO@users.noreply.github.com>
2026-07-10 01:22:20 +0800

    fix: use NotSupportedException in one-way WPF converters

--- UniversalDeviceToolkit.WPF/Utils/TextToVisibilityConverter.cs
+++ UniversalDeviceToolkit.WPF/Utils/TextToVisibilityConverter.cs
@@ -18,6 +18,6 @@ namespace UniversalDeviceToolkit.WPF.Utils;
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException("ConvertBack is not supported by this one-way converter.");
     }
 }
