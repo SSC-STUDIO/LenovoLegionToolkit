2b634cceaa4dda3dbdc4f9ebdaefee3a8b225533
root <root@localhost.localdomain>
2026-08-05 22:33:42 +0800

    test(avalonia): cover power mapping modes

--- UniversalDeviceToolkit.Tests/Avalonia/PowerSettingsServiceTests.cs
+++ UniversalDeviceToolkit.Tests/Avalonia/PowerSettingsServiceTests.cs
@@ -1,6 +1,7 @@
 #if WINDOWS
 
 using FluentAssertions;
+using UniversalDeviceToolkit.Lib;
 using UniversalDeviceToolkit.Avalonia.Services;
 using Xunit;
 
@@ -33,6 +34,49 @@ public sealed class PowerSettingsServiceTests
         powerModes.IsVisible.Should().Be(usesPowerMode);
         powerPlans.IsVisible.Should().Be(!usesPowerMode && !isDisabled);
         controlPanel.IsVisible.Should().Be(!usesPowerMode && !isDisabled);
+    }
+
+    [Fact]
+    public async Task PowerModeMapping_AllSharedValues_DisplayAndRoundTrip()
+    {
+        var service = AvaloniaSettingsServiceFactory.Create();
+        var initialPage = await service.GetPageAsync("Power");
+        var initialMapping = initialPage.Options.Single(option => option.Key == "PowerModeMapping");
+        var initialValue = initialMapping.SelectedValue;
+        var mappingValues = initialMapping.Values!;
+        var modes = Enum.GetValues<PowerModeMappingMode>();
+
+        mappingValues.Should().HaveCount(modes.Length);
+        mappingValues.Should().OnlyHaveUniqueItems();
+
+        try
+        {
+            foreach (var mode in modes)
+            {
+                await service.SetSelectionAsync("Power", "PowerModeMapping", mode.ToString());
+
+                var page = await service.GetPageAsync("Power");
+                var mapping = page.Options.Single(option => option.Key == "PowerModeMapping");
+
+                mapping.SelectedValue.Should().Be(mappingValues[(int)mode]);
+                mapping.Values.Should().Equal(mappingValues);
+
+                var powerModes = page.Options.Single(option => option.Key == "OpenPowerModes");
+                var powerPlans = page.Options.Single(option => option.Key == "OpenPowerPlans");
+                var controlPanel = page.Options.Single(option => option.Key == "OpenPowerPlansControlPanel");
+                if (mode == PowerModeMappingMode.Disabled)
+                {
+                    powerModes.IsVisible.Should().BeFalse();
+                    powerPlans.IsVisible.Should().BeFalse();
+                    controlPanel.IsVisible.Should().BeFalse();
+                }
+            }
+        }
+        finally
+        {
+            if (!string.IsNullOrWhiteSpace(initialValue))
+                await service.SetSelectionAsync("Power", "PowerModeMapping", initialValue);
+        }
     }
 }
 
